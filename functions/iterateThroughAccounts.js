997c7f33caace09c488d5725de17d518d130a94d
Dima Denisov <2denisov@bk.ru>
2021-09-25 15:35:38 +0300

    feat: Не проверяет tiktok_ws2

--- functions/iterateThroughAccounts.js
+++ functions/iterateThroughAccounts.js
@@ -3,24 +3,34 @@ import { writeLogToFile } from '../utils/writeLogToFile';
 import { checkAdCampaign } from './checkAdCampaign';
 
 async function iterateThroughAccounts(page, browser) {
-  // Получаем аккаунты
-  const accounts = await page.$$(
-    '.vi-collapse-item:first-child .custom-select-panel-item'
-  );
+  // // Получаем аккаунты
+  // const accounts = await page.$$(
+  //   '.vi-collapse-item:first-child .custom-select-panel-item'
+  // );
 
-  for await (const account of accounts) {
-    // Нажимает на селектор профилей
-    await page.click('.biz_user_warp');
-    await account.click();
-    // Получаем ссылки на профили, где были потрачены деньги
-    const accauntsWithCostsLinks = await getAccauntsWithCostsLinks(page);
-    writeLogToFile(
-      `\tВсего профилей с тратами: ${accauntsWithCostsLinks.length}\n`
-    );
-    // Проверяем рекламные кампании
-    for await (const link of accauntsWithCostsLinks) {
-      await checkAdCampaign(link, browser);
-    }
+  // for await (const account of accounts) {
+  //   // Нажимает на селектор профилей
+  //   await page.click('.biz_user_warp');
+  //   await account.click();
+  //   // Получаем ссылки на профили, где были потрачены деньги
+  //   const accauntsWithCostsLinks = await getAccauntsWithCostsLinks(page);
+  //   writeLogToFile(
+  //     `\tВсего профилей с тратами: ${accauntsWithCostsLinks.length}\n`
+  //   );
+  //   // Проверяем рекламные кампании
+  //   for await (const link of accauntsWithCostsLinks) {
+  //     await checkAdCampaign(link, browser);
+  //   }
+  // }
+
+  // Получаем ссылки на профили, где были потрачены деньги
+  const accauntsWithCostsLinks = await getAccauntsWithCostsLinks(page);
+  writeLogToFile(
+    `\tВсего профилей с тратами: ${accauntsWithCostsLinks.length}\n`
+  );
+  // Проверяем рекламные кампании
+  for await (const link of accauntsWithCostsLinks) {
+    await checkAdCampaign(link, browser);
   }
 }
 
